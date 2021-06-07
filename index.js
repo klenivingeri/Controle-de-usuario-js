fe8f630b39d4b4b15ae36c9e676cf23483726f53
Erick <libertti@live.com>
2021-06-07 11:35:18 -0300

    Trabalhando com innerHTML e appendChild

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ var user = {};
 var form = document.getElementById('form-user-create')
 form.addEventListener('submit', formData)
 
+
+
+function addLine(dataUser){
+    
+console.log(dataUser)
+    var tr = document.createElement('tr')
+    tr.innerHTML =`
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    `
+    document.getElementById('tbody').appendChild(tr)
+
+}
+
+
 function formData(e){
     
     fields.forEach(function(field, index){
@@ -15,9 +37,11 @@ function formData(e){
         }
 
     });
-    console.log(user);
-    e.preventDefault()
+    addLine(user)
+    e.preventDefault();
 }
+
+
 
 
 
